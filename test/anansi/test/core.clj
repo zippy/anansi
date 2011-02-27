021f0562199a351da931655e8ab534f4a502d817
Lewis Hoffman <lewis.hoffman@gmail.com>
2011-02-27 12:52:36 -0700

    add depencies to core.clj to facilitate compiling/running test suite via slime/swank

--- test/anansi/test/core.clj
+++ test/anansi/test/core.clj
@@ -1,3 +1,16 @@
 (ns anansi.test.core
+  (:use [ anansi.test.commands] :reload)
+  (:use [ anansi.test.receptor] :reload)
   (:use [anansi.core] :reload)
   (:use [clojure.test]))
+
+;; Until we learn better: this file should :use all the ns's with
+;; anansi tests in them.  Then all tests can be run in slime/swank by compiling
+;; this file and running (clojure.test/run-tests),  or, since that
+;; command seems to include running other namespaces, list them
+;; explicitly like so:
+;; (clojure.test/run-tests 'anansi.test.core 'anansi.test.commands 'anansi.test.receptor )
+
+;; This is mostly useful for quickly re-running the whole suite, eg
+;; for a big refactor.  For individual test or test files, use clojure-test-mode.
+
