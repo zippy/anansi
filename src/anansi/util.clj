53fc2f1a1e55bd5717b1525bfdda6e2ef3c75e07
Eric Harris-Braun <eric@harris-braun.com>
2011-05-11 23:42:13 -0400

    snapshot function for removing refs from map

--- src/anansi/util.clj
+++ src/anansi/util.clj
@@ -7,5 +7,19 @@
   [tform m]
   (into {} (map (fn [[k v]] { (tform k) v }) m)))
 
+(defn- do-snapshot
+  [m l]
+  (into {} (map (fn [[k v]]
+                  [k  (if (instance? clojure.lang.Ref v)
+                        (if (get l (str v))
+                          :skipping
+                          (if (map? @v)
+                            (do-snapshot @v (conj l (str v)))
+                            @v))
+                        v)]
+                  ) m)))
 
+(defn snapshot [m]
+  "removes all refs from a map"
+  (do-snapshot m #{}))
 
--- test/anansi/test/util.clj
+++ test/anansi/test/util.clj
@@ -1,7 +1,11 @@
 (ns anansi.test.util
-  (:use [anansi.util])
+  (:use [anansi.util] :reload)
   (:use [clojure.test]
         [clojure.contrib.io :only [writer]]))
 
 (deftest modify-keys-test
   (is (= {2 2 4 4} (modify-keys inc {1 2 3 4}))))
+
+(deftest snapshot-test
+  (is (= {:a 2, :b {4 5}}
+         (snapshot {:a 2, :b (ref {4 5})}))))
