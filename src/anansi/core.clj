1baf698c6a67d5d3899601bf76993a358decd215
Eric Harris-Braun <eric@harris-braun.com>
2011-05-31 15:12:18 -0400

    moved web-server code to its own file

--- src/anansi/core.clj
+++ src/anansi/core.clj
@@ -3,68 +3,13 @@
        :doc "A reference implementation of the ceptr platform"}
   anansi.core
   (:use [anansi.server :only [launch-server]]
-        [anansi.ceptr]
-        [anansi.user]
-        [anansi.receptor.host]
-        [anansi.receptor.scape]
-        [anansi.receptor.user]
-        [anansi.commands :only [execute]]
-        [anansi.user :only [user-streams]]
-        [anansi.server-constants :only [get-host]]
-        [clojure.contrib.json :only [json-str]]
-        )
-  (:use compojure.core, ring.adapter.jetty)
-  
-  (:require [compojure.route :as route]
-            [compojure.handler :as handler]
-            [ring.util.response :as response]))
+        [anansi.web-server :only [launch-web-server]]))
 
-(defn get-user
-  [username]
-  (dosync
-       (let [host (get-host)
-             users (contents host :user-scape)
-             user-address (s-> self->host-user host username) ;; creates or returns existing user receptor address
-             user (get-receptor host user-address)]
-         (--> key->set host users username user-address )
-         (--> self->connect host user :web)
-         (commute user-streams assoc username user)
-)))
-
-(defn view-form []
-  (str "<html><head></head><body>"
-       "<form method=\"post\">"
-       "Command <input type=\"text\" name=\"cmd\"/>"
-       "Data <input type=\"text\" name=\"data\"/>"
-       "Username <input type=\"text\" name=\"username\"/>"
-       "<input type=\"submit\"/>"
-       "</form></body></html>"))
-(defroutes main-routes
-  (GET "/cmd" [] (view-form))
-  (POST "/cmd" [cmd data username] (try
-                                  (if (or (= username "") (nil? username)) (throw (RuntimeException. "missing username")))
-                                  (binding [*user-name* username]
-                                    (get-user *user-name*)
-                                    (let [result 
-                                          (json-str (execute (if (or (= data "") (nil? data)) cmd (str cmd " " data))))]
-                                      (dosync (commute user-streams dissoc username))
-                                      result))
-                                  (catch Exception e
-                                    (.printStackTrace e *err*)
-                                    (str {:status :error
-                                          :result (str "exception raised: " e)}))))
-  (route/files "/" {:root "htdocs"})
-  (route/not-found "Page not found"))
-(def app
-     (handler/site main-routes))
-
-(defn -main    
-  ([port]
+(defn -main
+  ([cl-port web-port]
      (do 
-       (let [t (Thread. #(run-jetty app {:port 8080}))]
-         (doto t .start))
-       
-       (launch-server port)))
+       (launch-web-server web-port)       
+       (launch-server cl-port)))
   
-  ([] (-main 3333)))
+  ([] (-main 3333 8080)))
 
